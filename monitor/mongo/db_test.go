d0658edde0638c700b3361576bf1b77f2627b911
Andrew Braithwaite <andrew@losd.info>
2015-10-12 10:10:41 +0100

    add more mongo tests

--- monitor/mongo/db_test.go
+++ monitor/mongo/db_test.go
@@ -7,12 +7,28 @@ import (
 )
 
 func TestDial(t *testing.T) {
-	if os.Getenv("TRAVIS") != "true" {
-		t.Skip("skipping test - only runs on travis")
+	if os.Getenv("TEST_MONGO") != "true" {
+		t.Skip("skipping test - TEST_MONGO not set")
 	}
 
 	db, err := mongo.Db("localhost")
 	defer db.Close()
 
 	assert.Nil(t, err)
+}
+
+func TestGetMonitors(t *testing.T) {
+	if os.Getenv("TEST_MONGO") != "true" {
+		t.Skip("skipping test - TEST_MONGO not set")
+	}
+
+	db, err := mongo.Db("localhost")
+	defer db.Close()
+
+	assert.Nil(t, err)
+
+	healthchecks, err := db.GetMonitors()
+
+	assert.Nil(t, err)
+	assert.Equal(t, 2, len(healthchecks))
 }
