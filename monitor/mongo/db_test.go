839cb4651e2b3330409e1c98ab4bfb7c16449594
Andrew Braithwaite <andrew@losd.info>
2015-10-12 09:57:36 +0100

    only run mongo tests on travis

--- monitor/mongo/db_test.go
+++ monitor/mongo/db_test.go
@@ -3,9 +3,14 @@ import (
 	"testing"
 	"github.com/typingincolor/go-galen/monitor/mongo"
 	"github.com/stretchr/testify/assert"
+	"os"
 )
 
 func TestDial(t *testing.T) {
+	if os.Getenv("TRAVIS") != "true" {
+		t.Skip("skipping test - only runs on travis")
+	}
+
 	db, err := mongo.Db("localhost")
 	defer db.Close()
 
