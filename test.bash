cdae074c8dadf2bda4dbc47552ed506db704e19c
Andrew Braithwaite <typingincolor@users.noreply.github.com>
2015-10-12 21:03:43 +0100

    make it fail if the tests fail

--- test.bash
+++ test.bash
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+ERROR=0
 echo "mode: set" > acc.out
 for Dir in $(find ./* -maxdepth 10 -type d );
 do
         if ls $Dir/*.go &> /dev/null;
         then
             go test -coverprofile=profile.out $Dir
+            if [ "$?" -ne "0" ]
+            then
+                ERROR=1
+            fi
+            
             if [ -f profile.out ]
             then
                 cat profile.out | grep -v "mode: set" >> acc.out
             fi
 fi
 done
-goveralls -coverprofile=acc.out -service=travis-ci $COVERALLS; RESULT=$?
+goveralls -coverprofile=acc.out -service=travis-ci $COVERALLS
 rm -rf ./profile.out
 rm -rf ./acc.out
 
-exit $RESULT
+exit $ERROR
