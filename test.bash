7fec96a5786d3da853df1268db12803e23534ba5
Andrew Braithwaite <andrew@losd.info>
2015-10-12 09:12:29 +0100

    try to get coveralls to work

---
+++ test.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "mode: set" > acc.out
+for Dir in $(find ./* -maxdepth 10 -type d );
+do
+        if ls $Dir/*.go &> /dev/null;
+        then
+            go test -coverprofile=profile.out $Dir
+            if [ -f profile.out ]
+            then
+                cat profile.out | grep -v "mode: set" >> acc.out
+            fi
+fi
+done
+goveralls -coverprofile=acc.out -service=travis-ci $COVERALLS
+rm -rf ./profile.out
+rm -rf ./acc.out
