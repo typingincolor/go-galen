5b977a0411eb9f22902061ebef9edb1b938d647f
Andrew Braithwaite <abraithw@gmail.com>
2015-10-13 22:40:34 +0100

    Revert "try new old logging"
    This reverts commit d91f0b53d5e36f3e8cb6b934433e55dc4f7122b8.

--- monitor/gmonitor/main.go
+++ monitor/gmonitor/main.go
@@ -1,57 +1,27 @@
 package main
 
 import (
-	"log"
+	"gopkg.in/inconshreveable/log15.v2"
 	"os"
 	"os/signal"
 	"sync"
 	"syscall"
 	"time"
-	"io"
 
 	"flag"
 	"github.com/typingincolor/go-galen/monitor/mongo"
 	"github.com/typingincolor/go-galen/monitor/monitor"
 )
 
-var (
-    Trace   *log.Logger
-    Info    *log.Logger
-    Warning *log.Logger
-    Error   *log.Logger
-)
-
-func Init(
-    traceHandle io.Writer,
-    infoHandle io.Writer,
-    warningHandle io.Writer,
-    errorHandle io.Writer) {
-
-    Trace = log.New(traceHandle,
-        "TRACE: ",
-        log.Ldate|log.Ltime|log.Lshortfile)
-
-    Info = log.New(infoHandle,
-        "INFO: ",
-        log.Ldate|log.Ltime|log.Lshortfile)
-
-    Warning = log.New(warningHandle,
-        "WARNING: ",
-        log.Ldate|log.Ltime|log.Lshortfile)
-
-    Error = log.New(errorHandle,
-        "ERROR: ",
-        log.Ldate|log.Ltime|log.Lshortfile)
-}
+var logger = log15.New(log15.Ctx{"module": "main"})
 
 func main() {
-	Init(os.Stdout, os.Stdout, os.Stdout, os.Stderr)
 	var mongoHost = flag.String("mongo.host", "localhost", "Mongodb hostname")
 	var influxHost = flag.String("influx.host", "localhost", "Influxdb hostname")
 	var influxPort = flag.Int("influx.port", 8086, "Influxdb port")
 	flag.Parse()
 
-	Info.Println("Starting...")
+	logger.Info("Starting...")
 
 	var stoplock sync.Mutex
 	stop := false
@@ -62,14 +32,14 @@ func main() {
 		stoplock.Lock()
 		stop = true
 		stoplock.Unlock()
-		Info.Println("Stopping...")
+		logger.Info("Stopping...")
 		stopChan <- struct{}{}
 	}()
 	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
 
 	database, err := mongo.Db(*mongoHost)
 	if err != nil {
-		Error.Println("failed to dial MongoDB", err)
+		logger.Crit("failed to dial MongoDB", log15.Ctx{"error": err})
 		os.Exit(1)
 	}
 	defer database.Close()
